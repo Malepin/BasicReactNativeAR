a76d073f5e4448e52b01545af28e25ea1b0092b9
Malepin <pramuditamelvin@gmail.com>
2022-07-12 21:22:08 +0700

    fristcommit

---
+++ src/model/data.js
@@ -0,0 +1,21 @@
+
+import aviatorgold from '../src/assets/images/transparent/805289602057_0000.png';
+import aviatorblack from '../src/assets/images/transparent/black.png';
+import aviatorbrownmetal from '../src/assets/images/transparent/brown metal.png';
+import aviatorgreenmetal from '../src/assets/images/transparent/green metal.png';
+import aviatorgreymetal from '../src/assets/images/transparent/grey metal.png';
+import aviatorgunmetal from '../src/assets/images/transparent/gunmetal.png';
+import aviatorredmetal from '../src/assets/images/transparent/redmetal.png';
+
+
+export const data = {
+    productShowcase,
+    title: 'Ray Ban', 
+    subTitile: 'AVIATOR CLASSIC', 
+    price: 'Rp. 3.000.000', 
+    color: ['silver', '#F1D18A'], 
+    description: 'Currently one of the most iconic sunglass models in the world, Ray-Ban Aviator Classic sunglasses were originally designed for U.S. aviators in 1937. Aviator Classic sunglasses are a timeless model that combines great aviator styling with exceptional quality, performance and comfort',
+    listOfShowCase: [aviatorblack, aviatorbrownmetal, aviatorgold, aviatorgreenmetal, aviatorgreymetal, aviatorgunmetal, aviatorredmetal]
+}
+
+export default data
